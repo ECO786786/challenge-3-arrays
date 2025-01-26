b51752193727767e2e57187a629332c8041a8ca1
Emma Collins <lowpee56@hotmail.com>
2025-01-26 21:59:12 +0000

    find duplicates in an array'

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-//main file
+const findDuplicates = (array) => {
+  const duplicates = [];
+  const compare = [];
+  for (let num of array) {
+    if (!compare.includes(num)) {
+      compare.push(num);
+    } else {
+      duplicates.push(num);
+    }
+  }
+};
