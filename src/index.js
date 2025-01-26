612fdf4e5fee29a5d6b95540931e589a93931b9a
Emma Collins <lowpee56@hotmail.com>
2025-01-26 22:07:56 +0000

    remove duplicates in an array

--- src/index.js
+++ src/index.js
@@ -8,4 +8,12 @@ const findDuplicates = (array) => {
       duplicates.push(num);
     }
   }
+  return duplicates;
 };
+
+const removeDuplicates = (array) => {
+  return [...new Set(array)];
+};
+
+console.log(findDuplicates([1, 2, 2, 3, 4, 2, 1, 5, 6, 7, 8]));
+module.exports = { findDuplicates, removeDuplicates };
